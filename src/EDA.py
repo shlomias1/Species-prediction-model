6cee9fa1510306bc15adad74b9379894daf6f7a7
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-02-27 14:40:44 +0200

    Update EDA.py

--- src/EDA.py
+++ src/EDA.py
@@ -1,8 +1,32 @@
 import load_data
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def summary(df):
+    print(f"head: \n{df.head()}")
+    print(f"shape: {df.shape}")
+    print(f"columns: {df.columns}")
+    print({df.info()})
+    print(f"missing value each columns: \n{df.isnull().sum()}")
+    print(f"statictic: \n{df.describe()}")
+
+def check_categorial_columns(df):
+    categorical_cols = df.select_dtypes(include=["object"]).columns
+    for col in categorical_cols:
+        print(f"🔹 Column {col} - Number of unique categories: {df[col].nunique()}")
+        print(df[col].value_counts().head(10)) 
+        print("\n")
+
+def check_numeric_columns(df):
+    numeric_cols = df.select_dtypes(include=["int64", "float64"]).columns
+    for col in numeric_cols:
+        print(f"🔹 Column {col} - Number of unique values: {df[col].nunique()}")
+        print(f"Min: {df[col].min()} - Max: {df[col].max()}")
+        print("\n")
+           
 PA_train_data = load_data.load_data
-print(f"head: \n{PA_train_data.head()}")
-print(f"shape: {PA_train_data.shape}")
-print(f"columns: {PA_train_data.columns}")
-print({PA_train_data.info()})
+summary(PA_train_data)
+check_categorial_columns(PA_train_data)
+check_numeric_columns(PA_train_data)
